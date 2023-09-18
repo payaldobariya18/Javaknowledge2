fcdf7ccfb8a537b4dba0ed2464aa5f18d041c307
payal <payaldobariya18@gmail.com>
2023-09-18 16:25:17 +0100

    newly added

--- src/test1.java
+++ src/test1.java
@@ -2,5 +2,6 @@ public class test1 {
 
     public static void main(String[] args) {
         System.out.println("my name is mozilla.....");
+        System.out.println("mozilla firefox browser");
     }
 }
